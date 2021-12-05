aef7a71b3f25c1c992e8468ef29fc177772d6ca3
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-12-05 21:00:52 +0530

    Update code

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import pyrogram
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.types import User, Message
 import os
+from pyrogram import Client, filters
+
 
 DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
 
 bughunter0 = Client(
-    "name",
+    "Mp4-to-Mp3-Conveter",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
@@ -15,15 +13,32 @@ bughunter0 = Client(
 
 DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 
+
+@bughunter0.on_message(filters.private & filters.text)
+async def start(bot, message):
+    await message.reply_text("Send a video for converting to audio")
+
+
 @bughunter0.on_message(filters.video & filters.private)
 async def mp3(bot, message):
-    chat_id=message.chat.id
-    file_path = DOWNLOAD_LOCATION + f"{message.chat.id}.mp3"
-    txt = await message.reply("Downloading to My server.....")
-    await message.download(file_path)  
-    await txt.edit("Downloaded File")
-    await txt.edit("Converting to mp3")
-    await bot.send_audio(chat_id=chat_id,audio=file_path, caption="@BugHunterBots")
-    os.remove(file_path)
+    
+    # download video
+    file_path = DOWNLOAD_LOCATION + f"{message.from_user.id}.mp3"
+    txt = await message.reply_text("Downloading to My server.....")
+    await message.download(file_path)
+    await txt.edit_text("Downloaded Successfully")
+    
+    # convert to audio
+    await txt.edit_text("Converting to audio")
+    await message.reply_audio(audio=file_path, caption="@BugHunterBots", quote=True)
+    
+    # remove file
+    try:
+        os.remove(file_path)
+    except:
+        pass
+    
+    await txt.delete()
+
 
 bughunter0.run()
